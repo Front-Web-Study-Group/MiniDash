6e65e5b5b69e52088bb2f32d8be2121a1a9fc629
TaumuLu <972409545@qq.com>
2021-04-20 11:31:02 +0800

    fix: uppack docsets

--- lib/models/repos.dart
+++ lib/models/repos.dart
@@ -5,6 +5,7 @@ import 'package:mini_dash/utils/index.dart';
 import 'package:mini_dash/api/dash.dart';
 import 'package:xml/xml.dart';
 import 'package:archive/archive.dart';
+import 'package:path/path.dart' as path;
 
 class RepoDownload {
   static RepoDownload get instance => _getInstance();
@@ -20,31 +21,37 @@ class RepoDownload {
 
   factory RepoDownload() => _getInstance();
 
-  String docsetsPath = '/docsets/';
+  String docsetsPath = '/docsets';
 
-  Future<String> get downloadPath async {
-    var path = await getAppPath();
-    return path + docsetsPath;
+  Future<String> getStorePath([String cPath = ""]) async {
+    var appPath = await getAppPath();
+    return path.join(appPath, docsetsPath, cPath);
+  }
+
+  Future<String> getDownloadPath([String cPath = ""]) async {
+    var tempPath = await getTempPath();
+    return path.join(tempPath, docsetsPath, cPath);
   }
 
   RepoDownload._internal();
 
   unPack(String file) async {
-    // final bytes = File(file).readAsBytesSync();
-    // final archive = GZipDecoder().decodeBytes(bytes);
-    // final dPath = await downloadPath;
+    final bytes = File(file).readAsBytesSync();
+    final Archive archive =
+        new TarDecoder().decodeBytes(GZipDecoder().decodeBytes(bytes));
+    final dPath = await getStorePath();
 
-    // for (final file in archive) {
-    //   final filename = file.name;
-    //   if (file.isFile) {
-    //     final data = file.content as List<int>;
-    //     File(dPath + filename)
-    //       ..createSync(recursive: true)
-    //       ..writeAsBytesSync(data);
-    //   } else {
-    //     Directory(dPath + filename)..create(recursive: true);
-    //   }
-    // }
+    for (final file in archive) {
+      final filename = file.name;
+      if (file.isFile) {
+        final data = file.content as List<int>;
+        File(dPath + filename)
+          ..createSync(recursive: true)
+          ..writeAsBytesSync(data);
+      } else {
+        Directory(dPath + filename)..create(recursive: true);
+      }
+    }
   }
 
   download(String url, String name, {Function onReceiveProgress}) async {
@@ -61,8 +68,7 @@ class RepoDownload {
   downloads(RepoXml repoXml, {Function onReceiveProgress}) async {
     var urls = repoXml.urls;
     for (var url in urls) {
-      var tempPath = await getTempPath();
-      var file = '$tempPath/${url.split('/').last}';
+      var file = await getDownloadPath(url.split('/').last);
       try {
         var isExist = File(file).existsSync();
         if (!isExist) {
