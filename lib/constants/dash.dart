a62ef8da17f98c36232741d4f580aed31ee08bc7
zhi <linkxsystem@gmail.com>
2021-05-16 23:17:59 +0800

    feat: defined the variable of constants

---
+++ lib/constants/dash.dart
@@ -0,0 +1,9 @@
+/**
+ * @description Dash 的官方下载源
+ */
+
+// const String FEED_BASE_URL = "https://kapeli.com/feeds/";
+const String FEED_BASE_URL = "https://tokyo.kapeli.com/feeds/";
+const String FEED_BASE_URL_SPROUTCORE = "http://docs.sproutcore.com/feeds/";
+
+const String DOCSETS_PATH = "docsets";
---
+++ lib/constants/styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const K_PRIMARY_COLOR = Color(0xFF888888);
+
+const K_TEXT_COLOR = Color(0xFF888888);
+
+const K_BACKGROUND_COLOR = Color(0xFFF9F8FD);
+
+const K_CARD_BACKGROUND_COLOR = Color(0xFFDCDCDC);
+
+const K_SECONDARY_COLOR = Color(0xFFFE9901);
+
+const K_CONTENT_COLOR_LIGHT_THEME = Color(0xFF1D1D35);
+const K_CONTENT_COLOR_DARK_THEME = Color(0xFFF5FCF9);
+
+const K_WARNINNG_COLOR = Color(0xFFF3BB1C);
+const K_ERROR_COLOR = Color(0xFFF03738);
+
+const K_DEFAULT_PADDING = 20.0;
+
+const D_PRIMARY_COLOR = Color.fromARGB(255, 72, 148, 236);
